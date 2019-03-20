6b19f57f4ce66022cbd011dde24bb771034a3aa9
Adrian Kant <adjkant@gmail.com>
2019-03-20 07:39:36 -0400

    using struct-id class properly using syntax-classes package

--- json-sourcery/struct.rkt
+++ json-sourcery/struct.rkt
@@ -8,14 +8,14 @@
 
 (require (for-syntax racket/list
                      syntax/parse
-                     "struct-id.rkt"
+                     syntax/parse/class/struct-id
                      racket/syntax))
 
 ;; [Id-of Struct] -> [Maybe [List-of Symbol]]
 (define-syntax struct-field-names
   (syntax-parser
     [(_ id:struct-id)
-     #`(when/f #,(and (boolean? (syntax-e #'id.supertype-id)) (syntax-e #'id.supertype-id))
+     #`(when/f #,(and (boolean? (attribute id.supertype-id)) (attribute id.supertype-id))
                (let [(struct-name-length (add1 (string-length (symbol->string 'id))))]
                  (map string->symbol
                       (map (λ (a) (substring (symbol->string a) struct-name-length))
