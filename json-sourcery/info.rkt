7aa8a002733fc63c688b52ba8af91acf74375037
Adrian Kant <adjkant@gmail.com>
2019-03-20 07:43:33 -0400

    added dependency listing

--- json-sourcery/info.rkt
+++ json-sourcery/info.rkt
@@ -1,7 +1,7 @@
 #lang info
 
 (define collection "json-sourcery")
-(define deps '("base"))
+(define deps '("base" "syntax-classes"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/json-sourcery.scrbl" ())))
 (define pkg-desc "A library built on top of the json package for improving JSON serialization and
